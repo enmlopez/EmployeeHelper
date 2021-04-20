747d18fe9aff7f1d0926410321896b0318a9bf36
enmlopez <55348036+enmlopez@users.noreply.github.com>
2021-04-19 23:14:46 -0400

    Added test EmployeeListItem

---
+++ EmployeeHelper.Models/EmployeeModels/EmployeeListItem.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeHelper.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeHelper.Models.EmployeeModels
+{
+    public class EmployeeListItem
+    {
+        [Display(Name = "Employee ID")]
+        public int EmployeeId { get; set; }
+
+        [Display(Name = "First Name")]
+        [MaxLength(10, ErrorMessage = "Too many characters")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MaxLength(10, ErrorMessage = "Too many characters")]
+        public string LastName { get; set; }
+        public Shift Shift { get; set; }
+    }
+}
---
+++ EmployeeHelper/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeHelper.Models.EmployeeModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeHelper.Controllers
+{
+    [Authorize]
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var model = new EmployeeListItem[0];
+            return View(model); 
+        }
+    }
+}
